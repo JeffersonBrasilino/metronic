5ec6ae94e3b66cd1cf5b5e93e2b52a9f5c070ffb
jefferson brasilino <jefferson.wendhel@gmail.com>
2019-10-23 00:45:08 -0300

    mudanca de cor da barra de progresso http, animacao na tela de login.

--- dist/default/src/app/pages/auth/auth.component.ts
+++ dist/default/src/app/pages/auth/auth.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
 	selector: 'app-auth',
 	templateUrl: './auth.component.html',
 	styleUrls: ['./auth.component.scss'],
+	animations: [
+		trigger('showPage', [
+			transition('* <=> *', [
+				style({visibility: 'hidden',opacity: 0}),
+				animate('0.3s linear',style({visibility: 'visible',opacity: 1}))
+			])
+		])
+	]
 })
 export class AuthComponent implements OnInit {
+	show = false
 
 	constructor() { }
 
 	ngOnInit() {
+		this.show = true;
 	}
 
 }
